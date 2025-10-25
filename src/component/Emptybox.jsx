88ea397a2f8feeb2e8e69675a69b8dddfceb0f99
rahulgope45 <rahuleile456@gmail.com>
2025-10-25 19:12:50 +0530

    Added credentials

--- src/component/Emptybox.jsx
+++ src/component/Emptybox.jsx
@@ -5,7 +5,12 @@ function Emptybox() {
     <div
     className='bg-[#363C43]  from-[#4A4E54] to-[#1b1c1e] w-[700px] h-[689px] mt-7 rounded-[34px] flex items-center justify-center'>
       
-      Emptybox
+      <p className='text-white font-bold text-2xl'>
+        Name - Rahul Gope <br/>
+        Email - rahuleile456@gmail.com <br/>
+        Github - https://github.com/rahulgope45 <br/>
+        Portfolio -https://rahul-gope.vercel.app
+      </p>
     
     </div>
   )
